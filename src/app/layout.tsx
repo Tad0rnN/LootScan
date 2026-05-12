16fbc018be9c82c7d903a5e5b9761eb3583f42ab
Tad0rnN <tarikdoruk4@gmail.com>
2026-05-12 12:35:45 +0200

    chore: impact-site-verification meta etiketi eklendi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,16 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  // Başlık (title), açıklama (description) gibi diğer meta verilerinizi buraya ekleyebilirsiniz.
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return children;
+  return (
+    <html lang="en">
+      <head>
+        <meta name="impact-site-verification" {...{ value: "256553c0-22fe-4c4c-88be-3c7c5a474f69" }} />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
 }
